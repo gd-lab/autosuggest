b5694754ca6ac154161c1f22f3213b8dcb1006e3
dlabuzov <dlabuzov@gdspb0658.lupus.griddynamics.net>
2015-09-09 16:19:54 +0300

    new implementation

--- src/autocomplete.js
+++ src/autocomplete.js
@@ -15,8 +15,16 @@
         var suggestWeightsSorted = [];
 
         this.setValues = function(values) {
+            // trim keys:
+            values = _.mapKeys(values, function(value, key) { return _.trim(key); });
+            var phraseMinPriority = _.max(values) + 1;
+
             suggestMap = values;
             suggestWeightsSorted = _.sortBy(_.keys(values), function(key) {
+                if (key && ~key.indexOf(' ')) {
+                    // a special case - phrases have top priority
+                    return -(phraseMinPriority + values[key]);
+                }
                 return -values[key];
             });
         };
@@ -24,15 +32,55 @@
         this._getSortedValues = function() {
             return suggestWeightsSorted;
         };
+
+        this._getWeight = function(key) {
+            var result = suggestMap[key];
+            if (typeof result === "undefined") return -1;
+            return result;
+        };
     }
 
     Suggester.prototype.suggestValues = function(text) {
         if (!text) return null;
+        text = _.trimLeft(text).toLowerCase();
 
-        text = text.toLowerCase();
-        var maxItems = this._getOptions().maxItems;
-        var result = [];
+        var result;
+        var tagText;
+        var lastWordIndex = text.lastIndexOf(" ");
+        if (~lastWordIndex) {
+            if (lastWordIndex == (text.length-1)) return null;
+
+            // case: after whitespace entered and 1 new letter entered widget
+            //  should suggest new 8 best items for the 1 letter entered and
+            //  all previous words also.
+
+            result = this._getSuggestions(text);
+            text = text.substr(lastWordIndex + 1);
+            result = result.concat(this._getSuggestions(text));
+        } else {
+            // when the user types more than 2 letters the widget
+            //  should suggest up to 8 best elements by weight.
+
+            var tags = this.parseText(text);
+            if (!tags.length) return null;
+
+            tagText = tags[0].value;
+            if (tagText.length <= 2) {
+                return null;
+            }
+
+            result = this._getSuggestions(tagText);
+        }
+
+        return result;
+    };
+
+
+    Suggester.prototype._getSuggestions = function(text) {
         var vals = this._getSortedValues();
+        var maxItems = this._getOptions().maxItems;
+
+        var result = [];
         for (var i = 0, maxI = vals.length; i < maxI; i++) {
             var str = vals[i];
             if (~str.toLowerCase().indexOf(text)) {
@@ -40,6 +88,57 @@
                 if (result.length >= maxItems) break;
             }
         }
+
+        return result;
+    };
+
+    Suggester.prototype._findBestMatch = function(text, fullPhraseMatch) {
+        var vals = this._getSortedValues();
+
+        // find the best match (phrases matches first):
+        for (var i = 0, maxI = vals.length; i < maxI; i++) {
+            var str = vals[i];
+            if (_.startsWith(text, str.toLowerCase())) {
+                if (!fullPhraseMatch || (text.length === str.length ||
+                    (text.length > str.length && text[str.length] === " "))) {
+                    return str;
+                }
+            }
+        }
+
+        return null;
+    };
+
+    Suggester.prototype.parseText = function(text) {
+        if (!text) return [];
+
+        text = text.toLowerCase();
+
+        var maxItems = this._getOptions().maxItems;
+        var result = [];
+        var addToResult = (function(key) {
+            result.push({
+                "value": key,
+                "weight": this._getWeight(key)
+            });
+            return (result.length < maxItems);
+        }).bind(this);
+
+        while ((text = _.trim(text)).length) {
+            var bestMatch = this._findBestMatch(text, true);
+            if (bestMatch) {
+                text = text.substr(bestMatch.length);
+                if (!addToResult(bestMatch)) return result;
+            } else {
+                var last = text.indexOf(" ");
+                if (last < 0) {
+                    last = text.length;
+                }
+                if (!addToResult(text.substr(0, last))) return result;
+                text = text.substr(last);
+            }
+        }
+
         return result;
     };
 
@@ -67,6 +166,8 @@
             submitClass: "ac-submit",
             dialogClass: "ac-dialog",
             jsonUrl: "https://raw.githubusercontent.com/gd-lab/autosuggest/master/data/data.json",
+            sendToUrl: "http://www.somewhere.xyz/data/put",
+            onsendsuccess: function() {},
             onerror: function() {}
         };
 
@@ -89,65 +190,44 @@
         });
 
 
-        //var $el = $('#acTemplate').children().clone();  // TODO !!! rewrite with raw html
         var $el = _getAutocompleteWidgetCtrls();
-        this._$el = $el;
+        this.getElement = function() {
+            return $el;
+        };
+        var me = this;
 
         var $dialog = $('ul', $el).addClass(options.dialogClass);
         var $input = $('input', $el).addClass(options.inputClass);
         var $submit = $('button', $el).addClass(options.submitClass);
-//debugger
-
-        var currentTags = [];
-
-        this.getTags = function() {
-            return currentTags;
-        };
 
         var currentInputText = "";
-        var currentEditingTagIndex = -1;
         var onInputChange = _.debounce(function() {
-            //_dialogHide();
-
             var inputText = $input.val();
             if (currentInputText === inputText) return;
 
-            var tags = inputText.split(",");
-            currentEditingTagIndex = findLastChangedRowIndex(currentTags, tags);
-            var valToSuggest = tags[currentEditingTagIndex];
+            tryToSuggest(inputText);
 
-            console.log(valToSuggest, currentTags, tags);
-            tryToSuggest(valToSuggest);
-
-            currentTags = tags;
             currentInputText = inputText;
-        }, 1);
-        $input//.on('keypress', function(e) { if (e.which == 44/*,*/) return false; })
-            .on('change keydown keypress', onInputChange);
+        }, 10);
+        $input.on('change keydown keypress', onInputChange);
 
-        function replaceCurrentTag(tag) {
-            /*var inputText = $input.val();
-            inputText = inputText.replace()*/
-            currentTags[currentEditingTagIndex] = tag;
-            var inputText = currentTags.join(",");
+        this.getTags = function() {
+            return suggester.parseText($input.val());
+        };
+
+        function replaceInputText(selectedText) {
+            var inputText = $input.val();
+
+            var lastWordIndex = inputText.lastIndexOf(" ");
+            if (~lastWordIndex) {
+                inputText = inputText.substr(0, lastWordIndex + 1) + selectedText;
+            } else {
+                inputText = selectedText;
+            }
+
             $input.val(inputText);
             currentInputText = inputText;
-        }
-
-        function findLastChangedRowIndex(oldArr, newArr) {
-            var oldArrIdx = oldArr.length;
-            var newArrIdx = newArr.length;
-            while (oldArrIdx >= 0 && newArrIdx >= 0) {
-                if (oldArr[oldArrIdx] !== newArr[newArrIdx]) {
-                    return newArrIdx;
-                }
-                oldArrIdx--;
-                newArrIdx--;
-            }
-            if (newArrIdx >= 0) {
-                return newArrIdx;
-            }
-            return -1;
+            console.table(me.getTags());
         }
 
         function tryToSuggest(text) {
@@ -155,10 +235,6 @@
             if (!text) return;
 
             text = _.trimLeft(text);
-            if (text.length <= 2) {
-                return;
-            }
-
             var result = suggester.suggestValues(text);
             if (!result || !result.length) return;
 
@@ -178,15 +254,30 @@
         }
 
         $dialog.on("click", "li", function _selectValue(e) {
-            //debugger
-            replaceCurrentTag(this.innerText);
+            replaceInputText(this.innerText);
             _dialogHide();
+            $input.focus();
         });
 
-    };
 
-    AutocompleteWidget.prototype.getElement = function() {
-        return this._$el;
+        function _onSubmit() {
+            var result = me.getTags();
+            console.table(result);
+
+            $.ajax({
+                type: "POST",
+                dataType: "json",
+                url: options.sendToUrl,
+                data: JSON.stringify(result)
+            }).done(function (data) {
+                options.onsendsuccess(data);
+            }).fail(function() {
+                alert("Ajax failed to post data");
+                options.onerror();
+            });
+        };
+
+        $submit.on('click', _onSubmit);
     };
 
 
