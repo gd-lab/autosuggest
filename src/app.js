898ea49ed878664f71e1ee0d79ae845543672fc6
dlabuzov <dlabuzov@gdspb0658.lupus.griddynamics.net>
2015-09-10 17:16:08 +0300

    sources are splitted into modules

---
+++ src/app.js
@@ -0,0 +1,10 @@
+requirejs.config({
+    "baseUrl": "./src",
+    "paths": {
+      "jquery": "https://code.jquery.com/jquery-1.11.3.min",
+      "lodash": "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/3.10.1/lodash.min"
+    }
+});
+
+// Load the main app module to start the app
+requirejs(["./main"]);
---
+++ src/main.js
@@ -0,0 +1,24 @@
+define(["jquery", "./autocomplete/autocomplete"], function ($, AutocompleteWidget) {
+
+    $(document).ready(function() {
+
+        $.ajaxSetup({
+            beforeSend: function() {
+               $('#loading').show();
+            },
+            complete: function(){
+               $('#loading').hide();
+            },
+            success: function() {}
+        });
+
+        var widget = new AutocompleteWidget({
+            inputClass: "ac-input",
+            sendToUrl: "http://www.somewhere.xyz/data/put"
+        });
+
+        $("#container").append(widget.getElement());
+
+    });
+
+});
